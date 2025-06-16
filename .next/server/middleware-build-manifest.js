3f875175bc0a99fa15410e9187e58457ff03c265
nataliepham25 <nataliepham525@gmail.com>
2025-06-16 14:45:49 -0400

    Deploy to user pages - remove basePath and update config

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-f4a6d1ab548c7d92.js","static/chunks/4bd1b696-5b6c0ccbd3c0c9ab.js","static/chunks/684-2d20e3f2351b8ccb.js","static/chunks/main-app-cc838062b5a275f5.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-f4a6d1ab548c7d92.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-0c567ce1b85b045b.js","static/chunks/pages/_app-da15c11dea942c36.js"],"/_error":["static/chunks/webpack-f4a6d1ab548c7d92.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-0c567ce1b85b045b.js","static/chunks/pages/_error-cc3f077a18ea1793.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
     output: "export",
     trailingSlash: true,
-    basePath: "/nataliepham",
-    assetPrefix: "/nataliepham",
     eslint: {
         ignoreDuringBuilds: true,
     },
